baa157f99e2e7227d080b34cce2dfe96512ec60d
Mohamed Hamed <mo7amed7amed2030@outlook.com>
2020-11-24 11:40:48 +0200

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            string name = "Mohamed";
+            ViewData["name"] = name;
+            return View();
+        }
+
+        public ViewResult contact()
+        {
+            string msg = "Welcome from Contact";
+            string name = "Mohamed";
+            int phone = 01145708978;
+            string mail = "m@m.com";
+
+            ViewData["msg"] = msg;
+            ViewData["name"] = name;
+            ViewData["phone"] = phone;
+            ViewData["mail"] = mail;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public string Hello() 
+        {
+            return "Hello, Welcome To MVC";
+
+            Dictionary <int, string> names = new Dictionary<int, string>();
+            names.Add(1, "Mohamed");
+            names.Add(2, "Ahmed");
+
+            foreach (KeyValuePair<int, string> item in (names));
+
+        }
+        public int Add(int id=10)
+        {
+            return id+10;
+        }
+        public int Mul(int id = 10)
+        {
+            return id * 10;
+        }
+    }
+}
